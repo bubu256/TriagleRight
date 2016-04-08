29be52bd6801e0062ca34d1ac5fb9d4a32d09116
bu256 <bulostm@gmail.com>
2016-04-08 11:09:36 +0300

    First

---
+++ RightTriagl_Project/RightTriagle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightTriagl_Project
+{
+    public class RightTriagle
+    {
+        public static double Area(double a, double b, double hypotenuse)
+        {
+            double delta = 0.001;
+            if ( Math.Abs((hypotenuse * hypotenuse) - (a * a + b * b)) > delta )
+                throw new System.ArgumentException("Некорректно заданы параметры: Cумма квадратов катетов должна быть равна квадрату гипотенузы");
+            if ((a<=0)||(b<=0)||(hypotenuse<=0))
+                throw new System.ArgumentException("Некорректно заданы параметры: Все стороны треугольника должны быть больше нуля"); 
+            
+            return a * b / 2;
+        } 
+    }
+}
---
+++ RightTriagleTest/RightTriagleTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RightTriagl_Project;
+
+namespace RightTriagleTest
+{
+    [TestClass]
+    public class RightTriagleTest
+    {
+        [TestMethod]
+        public void AreaTest_a2b4()
+        {
+            double a = 2;
+            double b = 4;
+            double c = 4.4721;
+                
+            double expected = 4;
+            double actual = RightTriagle.Area(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AreaTest_ArgumentExceptionMinusA()
+        {
+            double a = -2;
+            double b = 4;
+            double c = 4.4721;
+
+            RightTriagle.Area(a, b, c);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AreaTest_ArgumentExceptionMinusB()
+        {
+            double a = 2;
+            double b = -4;
+            double c = 4.4721;
+
+            RightTriagle.Area(a, b, c);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AreaTest_ArgumentExceptionMinusC()
+        {
+            double a = 2;
+            double b = 4;
+            double c = -4.4721;
+
+            RightTriagle.Area(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AreaTest_ArgumentException0()
+        {
+            double a = 0;
+            double b = 4;
+            double c = 4.4721;
+
+            RightTriagle.Area(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AreaTest_ArgumentExceptionNotRight()
+        {
+            double a = 2;
+            double b = 4;
+            double c = 20;
+
+            double expected = 4;
+            double actual = RightTriagle.Area(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
